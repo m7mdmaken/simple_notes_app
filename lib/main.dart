7e08b277c2184f70ec89877b65fd60021012cd6a
mohamed almaken <96602477+m7mdmaken@users.noreply.github.com>
2024-09-24 01:46:45 +0300

    hive adapter for note model

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:simple_notes_app/views/notes_view.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 
-void main() {
+void main() async {
+  await Hive.initFlutter();
+  await Hive.openBox('settings');
   runApp(const SimpleNotesApp());
 }
 
--- lib/models/note_model.dart
+++ lib/models/note_model.dart
@@ -1,7 +1,16 @@
-class NoteModel {
+import 'package:hive/hive.dart';
+
+part 'note_model.g.dart';
+
+@HiveType(typeId: 1)
+class NoteModel extends HiveObject {
+  @HiveField(1)
   final String title;
+  @HiveField(2)
   final String note;
+  @HiveField(3)
   final String date;
+  @HiveField(4)
   final int color;
   NoteModel(
       {required this.color,
