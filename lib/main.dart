5ed5877661d5665f15ff835b169f2857e031de1e
mohamed almaken <96602477+m7mdmaken@users.noreply.github.com>
2024-09-23 19:24:34 +0300

    CUSTOM APP BAR

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,7 @@ class SimpleNotesApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+        debugShowCheckedModeBanner: false,
         home: const NotesView(),
         theme: ThemeData(
           brightness: Brightness.dark,
--- lib/widgets/custom_body.dart
+++ lib/widgets/custom_body.dart
@@ -1,10 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:simple_notes_app/widgets/custom_appbar.dart';
 
 class CustomBody extends StatelessWidget {
   const CustomBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const Padding(
+      padding: EdgeInsets.symmetric(horizontal: 24),
+      child: Column(
+        children: [
+          CustomAppBar(),
+          Text('This is a custom body widget.'),
+        ],
+      ),
+    );
   }
 }
