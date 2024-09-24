6caf9b62cb342d529c8034a24f4e85c73bfa3954
mohamed almaken <96602477+m7mdmaken@users.noreply.github.com>
2024-09-25 02:13:47 +0300

    initialize addnote cubit

--- lib/cubits/add_notes_cubit/add_note_cubit.dart
+++ lib/cubits/add_notes_cubit/add_note_cubit.dart
@@ -1,4 +1,5 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 import 'package:simple_notes_app/cubits/add_notes_cubit/add_note_state.dart';
 import 'package:simple_notes_app/models/note_model.dart';
 
@@ -7,5 +8,13 @@ class AddNoteCubit extends Cubit<AddNoteState> {
 
   // final NotesRepository _notesRepository;
 
-  void addNote(NoteModel note) {}
+  void addNote(NoteModel note) {
+    try {
+      var notesBox = Hive.box<NoteModel>('notes_box');
+      notesBox.add(note);
+      emit(AddNoteInitial());
+    } catch (e) {
+      emit(AddNoteFailure(e.toString()));
+    }
+  }
 }
