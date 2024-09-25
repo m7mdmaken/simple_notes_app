15c385dc8c77f81312de215836a3dd2455a10fae
mohamed almaken <96602477+m7mdmaken@users.noreply.github.com>
2024-09-25 17:18:19 +0300

    add addnote consumer

--- lib/widgets/custom_bottom_sheet.dart
+++ lib/widgets/custom_bottom_sheet.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
+import 'package:simple_notes_app/cubits/add_notes_cubit/add_note_cubit.dart';
+import 'package:simple_notes_app/cubits/add_notes_cubit/add_note_state.dart';
 import 'package:simple_notes_app/widgets/add_note_form.dart';
 
 class CustomBottomSheet extends StatelessWidget {
@@ -9,10 +13,28 @@ class CustomBottomSheet extends StatelessWidget {
     return Center(
       child: Padding(
         padding: const EdgeInsets.only(top: 48, left: 20, right: 20),
-        child: ListView(
-            shrinkWrap: true,
-            physics: const NeverScrollableScrollPhysics(),
-            children: const [AddNoteForm()]),
+        child: BlocConsumer<AddNoteCubit, AddNoteState>(
+          listener: (context, state) {
+            if (state is AddNoteSuccess) {
+              Navigator.pop(context);
+            } else if (state is AddNoteFailure) {
+              ScaffoldMessenger.of(context).showSnackBar(
+                SnackBar(
+                  content: Text(state.errMessage),
+                ),
+              );
+            }
+          },
+          builder: (context, state) {
+            return ModalProgressHUD(
+              inAsyncCall: (state is AddNoteLoading),
+              child: ListView(
+                  shrinkWrap: true,
+                  physics: const NeverScrollableScrollPhysics(),
+                  children: const [AddNoteForm()]),
+            );
+          },
+        ),
       ),
     );
   }
